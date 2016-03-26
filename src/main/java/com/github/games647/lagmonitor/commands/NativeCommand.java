60d6b696817cb121ba864b0a7fda9b68dc8bf46c
games647 <games@tsprivat.de>
2016-03-26 20:40:18 +0100

    Only load the native library if it's used

--- src/main/java/com/github/games647/lagmonitor/commands/NativeCommand.java
+++ src/main/java/com/github/games647/lagmonitor/commands/NativeCommand.java
@@ -10,6 +10,8 @@ import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.hyperic.jni.ArchLoader;
+import org.hyperic.jni.ArchNotSupportedException;
 import org.hyperic.sigar.CpuInfo;
 import org.hyperic.sigar.CpuPerc;
 import org.hyperic.sigar.FileSystemUsage;
@@ -17,14 +19,11 @@ import org.hyperic.sigar.Mem;
 import org.hyperic.sigar.NetInterfaceStat;
 import org.hyperic.sigar.Sigar;
 import org.hyperic.sigar.SigarException;
-import org.hyperic.sigar.SigarLoader;
 
 public class NativeCommand implements CommandExecutor {
 
     private static final ChatColor PRIMARY_COLOR = ChatColor.DARK_AQUA;
     private static final ChatColor SECONDARY_COLOR = ChatColor.GRAY;
-
-    private static final String DRIVER_NAME = SigarLoader.getNativeLibraryName();
 
     private final LagMonitor plugin;
 
@@ -43,10 +42,21 @@ public class NativeCommand implements CommandExecutor {
         }
 
         //we have to check this manually because the load statement in sigar is static
-        File driverFile = new File(plugin.getDataFolder(), DRIVER_NAME);
-        if (!driverFile.exists()) {
+        String driverName = null;
+        try {
+            ArchLoader archLoader = new ArchLoader();
+            archLoader.setName("sigar");
+            driverName = ArchLoader.getLibraryPrefix()
+                    + archLoader.getArchLibName()
+                    + ArchLoader.getLibraryExtension();
+        } catch (ArchNotSupportedException archNotSupportedException) {
+            plugin.getLogger().log(Level.SEVERE, null, archNotSupportedException);
+        }
+
+        File driverFile = new File(plugin.getDataFolder(), driverName);
+        if (driverFile == null || !driverFile.exists()) {
             sender.sendMessage(ChatColor.DARK_RED + "Couldn't find driver in the plugin folder");
-            sender.sendMessage(ChatColor.DARK_RED + "You need: " + DRIVER_NAME);
+            sender.sendMessage(ChatColor.DARK_RED + "You need: " + driverName);
             sender.sendMessage(ChatColor.DARK_RED + "and place it here: " + plugin.getDataFolder().getPath());
             return true;
         }
@@ -72,7 +82,7 @@ public class NativeCommand implements CommandExecutor {
             long actualUsed = mem.getActualUsed();
             long used = mem.getUsed();
 
-            long cache = actualUsed - used;
+            long cache = used - actualUsed;
             sender.sendMessage(PRIMARY_COLOR + "Memory Cache: " + SECONDARY_COLOR + Sigar.formatSize(cache));
 
             //net upload download
@@ -87,13 +97,12 @@ public class NativeCommand implements CommandExecutor {
             }
 
             if (usedNetInterfaceStat != null) {
-                sender.sendMessage(PRIMARY_COLOR + "Net name" + SECONDARY_COLOR + usedNetInterfaceStat);
-                sender.sendMessage(PRIMARY_COLOR + "Net speed" + SECONDARY_COLOR + usedNetInterfaceStat.getSpeed());
+                sender.sendMessage(PRIMARY_COLOR + "Net speed: " + SECONDARY_COLOR + usedNetInterfaceStat.getSpeed());
 
                 long receivedBytes = usedNetInterfaceStat.getRxBytes();
                 long sentBytes = usedNetInterfaceStat.getTxBytes();
-                sender.sendMessage(PRIMARY_COLOR + "Net Received" + SECONDARY_COLOR + Sigar.formatSize(receivedBytes));
-                sender.sendMessage(PRIMARY_COLOR + "Net Sent" + SECONDARY_COLOR + Sigar.formatSize(sentBytes));
+                sender.sendMessage(PRIMARY_COLOR + "Net Rec: " + SECONDARY_COLOR + Sigar.formatSize(receivedBytes));
+                sender.sendMessage(PRIMARY_COLOR + "Net Sent: " + SECONDARY_COLOR + Sigar.formatSize(sentBytes));
             }
 
             //*should* already included in environment - because its querable with mbeans too
@@ -109,10 +118,10 @@ public class NativeCommand implements CommandExecutor {
             FileSystemUsage fileSystemUsage = sigar.getFileSystemUsage(rootFileSystem);
             long diskReadBytes = fileSystemUsage.getDiskReadBytes();
             long diskWriteBytes = fileSystemUsage.getDiskWriteBytes();
-            sender.sendMessage(PRIMARY_COLOR + "Disk Read" + SECONDARY_COLOR + Sigar.formatSize(diskReadBytes));
-            sender.sendMessage(PRIMARY_COLOR + "Disk Write" + SECONDARY_COLOR + Sigar.formatSize(diskWriteBytes));
-        } catch (SigarException ex) {
-            plugin.getLogger().log(Level.SEVERE, null, ex);
+            sender.sendMessage(PRIMARY_COLOR + "Disk Read: " + SECONDARY_COLOR + Sigar.formatSize(diskReadBytes));
+            sender.sendMessage(PRIMARY_COLOR + "Disk Write: " + SECONDARY_COLOR + Sigar.formatSize(diskWriteBytes));
+        } catch (SigarException sigarException) {
+            plugin.getLogger().log(Level.SEVERE, null, sigarException);
         } finally {
             sigar.close();
         }
